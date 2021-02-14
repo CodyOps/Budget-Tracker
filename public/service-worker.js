5523fb1c1b8c29a801ab3b357b8ff8b09dd859aa
Cody Chen <codychen1@gmail.com>
2021-02-14 12:30:55 -0700

    added cache removal to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,17 +7,34 @@ const FILES_TO_CACHE = [
   "/manifest.json",
 ];
 
-const Cache_Name = "static-cache-v2";
-const Data_Cache_Name = "data-cache-v1";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 //Install
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(Cache_Name).then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       //return to the console if the files were cached
-      console.log("Your files were pre-cached successfully!");
+      console.log("Your files were successfully pre-cached!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            //return to the console of old cache removal
+            console.log("Removing old cache!", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
