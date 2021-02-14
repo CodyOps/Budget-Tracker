c300ce82a5c20462e4bb51db56c82c11f5bd88a9
Cody Chen <codychen1@gmail.com>
2021-02-14 12:22:05 -0700

    added to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,3 +6,18 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/manifest.json",
 ];
+
+const Cache_Name = "static-cache-v2";
+const Data_Cache_Name = "data-cache-v1";
+
+//Install
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(Cache_Name).then((cache) => {
+      //return to the console if the files were cached
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
